639e1e767b7c53824c6521fb6a400b975ef8219d
Harshit Prasad <harshitprasad532@gmail.com>
2023-10-01 17:33:03 +0530

    Added middlware in the route checking for unauthorized access

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  registerUser,
+  getAllUsers,
+} from "../controllers/user.controller.js";
+import { protect } from "../middlewares/authentication.middleware.js";
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, getAllUsers);
 router.post("/login", loginUser);
 
 export { router };
