969167db2daab4dee487925c884c039ff4c20b50
Harshit Prasad <harshitprasad532@gmail.com>
2023-12-31 19:30:09 +0530

    updated cors

--- server/src/server.js
+++ server/src/server.js
@@ -22,7 +22,7 @@ const whiteList = [
 ];
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whiteList.indexOf(origin) !== -1 || !origin) {
+    if (whiteList.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
