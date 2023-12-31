3898e41c2fca2b9af1ccdb02d014bb88c3e2515f
Harshit Prasad <harshitprasad532@gmail.com>
2023-12-31 18:00:19 +0530

    deployment

--- client/src/slices/api/apiSlice.js
+++ client/src/slices/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000",
+  baseUrl: "https://converse-api.onrender.com",
   prepareHeaders(headers, { getState }) {
     const authenticationToken = getState().authentication.userInformation;
     if (authenticationToken)
